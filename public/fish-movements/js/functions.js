3cbc99aec227f2cb300ae3f099cbeff29e2677bc
Ben Letcher <ben@felek.cns.umass.edu>
2016-09-28 15:37:24 -0400

    added loading spinner to movements app

--- public/fish-movements/js/functions.js
+++ public/fish-movements/js/functions.js
@@ -1214,6 +1214,18 @@ function fill2D(rows, cols,i) {
   return array;
 }
 
+function hideLoading () {
+  d3.select('#loading')
+    .style('opacity', 1)
+    .transition()
+    .duration(1000)
+    .style('opacity', 0)
+    .on('end', function () {
+      // after transition, hide element
+      d3.select(this).style('display', 'none');
+  });
+}
+
 
 /*** Copyright 2013 Teun Duynstee Licensed under the Apache License, Version 2.0 ***/
 firstBy=function(){function n(n){return n}function t(n){return"string"==typeof n?n.toLowerCase():n}function r(r,e){if(e="number"==typeof e?{direction:e}:e||{},"function"!=typeof r){var i=r;r=function(n){return n[i]?n[i]:""}}if(1===r.length){var u=r,o=e.ignoreCase?t:n;r=function(n,t){return o(u(n))<o(u(t))?-1:o(u(n))>o(u(t))?1:0}}return-1===e.direction?function(n,t){return-r(n,t)}:r}function e(n,t){var i="function"==typeof this?this:!1,u=r(n,t),o=i?function(n,t){return i(n,t)||u(n,t)}:u;return o.thenBy=e,o}return e}();
