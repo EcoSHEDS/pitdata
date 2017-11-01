9055f15e130a6ce8be2fccd34d3990e5d1690d95
bletcher <bletcher@eco.umass.edu>
2017-11-01 13:03:49 -0400

    reordered panels, add select by species

--- public/fish-movements/js/functions.js
+++ public/fish-movements/js/functions.js
@@ -875,6 +875,19 @@ function clickDot(){
 
  }
  
+ else if (state.onClick == "spp") {
+
+   // Empty selectedID array
+   state.selectedID = [];
+   
+   // get all data from the species of the selected individual
+   state.speciesData = getDataSpecies(state.nodesRender,d.species);
+
+   // Add ID's of the selected fish's species to selectedID
+   state.selectedID = state.speciesData.map( function(d){ return(d.id) } );
+
+ }
+ 
  updateRenderData();
  ticked();
  ended(); 
@@ -1309,10 +1322,6 @@ function startTour () {
   })
   intro.start();
 }
-
-//  $('#collapseTwo').collapse('toggle')
-//  $('#collapseThree').collapse('toggle')
-//  $('#collapseFour').collapse('toggle')
 
 /*** Copyright 2013 Teun Duynstee Licensed under the Apache License, Version 2.0 ***/
 firstBy=function(){function n(n){return n}function t(n){return"string"==typeof n?n.toLowerCase():n}function r(r,e){if(e="number"==typeof e?{direction:e}:e||{},"function"!=typeof r){var i=r;r=function(n){return n[i]?n[i]:""}}if(1===r.length){var u=r,o=e.ignoreCase?t:n;r=function(n,t){return o(u(n))<o(u(t))?-1:o(u(n))>o(u(t))?1:0}}return-1===e.direction?function(n,t){return-r(n,t)}:r}function e(n,t){var i="function"==typeof this?this:!1,u=r(n,t),o=i?function(n,t){return i(n,t)||u(n,t)}:u;return o.thenBy=e,o}return e}();
